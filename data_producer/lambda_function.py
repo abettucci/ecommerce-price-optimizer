b05a0069291a8316bae0325bcc1bd927d5bb2b92
abettucci <89608756+abettucci@users.noreply.github.com>
2023-11-27 22:34:22 -0300

    Update lambda_function.py

--- data_producer/lambda_function.py
+++ data_producer/lambda_function.py
@@ -1,19 +1,19 @@
 import json
-import pandas as pd
-import requests
-import statistics
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-import time
-from datetime import datetime, timedelta
-from gspread.utils import rowcol_to_a1
-from gspread.exceptions import APIError
-import pytz
-from cachetools import cached, TTLCache
-from googleapiclient.errors import HttpError
-import boto3
-from botocore.exceptions import ClientError
-from opensearchpy import OpenSearch
+# import pandas as pd
+# import requests
+# import statistics
+# import gspread
+# from oauth2client.service_account import ServiceAccountCredentials
+# import time
+# from datetime import datetime, timedelta
+# from gspread.utils import rowcol_to_a1
+# from gspread.exceptions import APIError
+# import pytz
+# from cachetools import cached, TTLCache
+# from googleapiclient.errors import HttpError
+# import boto3
+# from botocore.exceptions import ClientError
+# from opensearchpy import OpenSearch
 
 def lambda_handler(event, context):
 
